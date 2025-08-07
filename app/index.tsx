6bdf1c117d06cb9ef3c692de0cdf90c829b37daa
Obed Tawiah <tobed1586@gmail.com>
2025-08-07 23:06:34 +0000

    Day 1 react native create a new react native app  and adding tailwind css

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+   className="flex-1 justify-center items-center bg-blue-400"
+    >
+      <Text className="text-3xl font-bold ">Welcome to React Native </Text>
+    </View>
+  );
+}
